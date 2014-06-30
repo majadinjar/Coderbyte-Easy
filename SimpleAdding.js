662551a18eafc150ffe7365ef04dc4f298495f90
majadinjar <majadinjar@users.noreply.github.com>
2014-06-30 16:21:38 +0200

    Create SimpleAdding.js

---
+++ SimpleAdding.js
@@ -0,0 +1,15 @@
+function SimpleAdding(num) { 
+  
+  var result = 0;
+  for(var i = 1; i<=num;i++)
+  {
+    result+=i;
+  }
+  // code goes here  
+  return result; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+SimpleAdding(readline()); 
