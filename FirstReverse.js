c70dc15750864a8ff76f2ea9e8a5fff54efe65fa
majadinjar <majadinjar@users.noreply.github.com>
2014-06-30 16:09:29 +0200

    Create FirstReverse.js

---
+++ FirstReverse.js
@@ -0,0 +1,10 @@
+function FirstReverse(str) { 
+    var result = str.split("").reverse().join("");
+  // code goes here  
+  return result; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstReverse(readline());           
