d7e2bf66c6e6612a1d716ce612e389d389e342e1
majadinjar <majadinjar@users.noreply.github.com>
2014-06-30 16:12:38 +0200

    Create AlphabetSoup.js

---
+++ AlphabetSoup.js
@@ -0,0 +1,17 @@
+function AlphabetSoup(str) {
+   var alphabet = "abcdefghijklmnopqrstuvwxyz";
+   var string = "";
+   var indices = [];
+   for (var i = 0; i < str.length; i++) {
+      indices.push(alphabet.indexOf(str[i]));
+   }
+   indices.sort(function(a,b) {return b-a});
+   while (indices.length > 0) {
+      string += alphabet.charAt(indices.pop());
+   }
+   return string;
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+AlphabetSoup(readline()); 
