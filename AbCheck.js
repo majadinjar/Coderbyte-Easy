ebb99c120102871d76891b98acb94ba40daeb0ca
majadinjar <majadinjar@users.noreply.github.com>
2014-06-30 16:25:52 +0200

    Update AbCheck.js

--- AbCheck.js
+++ AbCheck.js
@@ -3,13 +3,13 @@ function ABCheck(str) {
   str = str.toLowerCase();
   var az = [], bz = [], ok = false;
   
-  for (var i = 0; i < str.length; i++) {
+  for (var i=0; i<str.length; i++) {
     if (str[i] == 'a') az.push(i);
     else if (str[i] == 'b') bz.push(i);
   }
   
-  for (var i = 0; i < az.length; i++) {
-    for (var j = 0; j < bz.length; j++) {
+  for (var i=0; i<az.length; i++) {
+    for (var j=0; j<bz.length; j++) {
       if (Math.abs(az[i] - bz[j]) - 1 == 3) {
         ok = true;
       }
