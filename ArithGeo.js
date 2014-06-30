ccbe5125d9e7bdb9ab0b30f764d1d4017f29b96c
majadinjar <majadinjar@users.noreply.github.com>
2014-06-30 16:13:13 +0200

    Create ArithGeo.js

---
+++ ArithGeo.js
@@ -0,0 +1,44 @@
+function ArithGeo(arr) { 
+      var x = 0;
+      var s = 0;
+      var result = '';
+      var allObjects = [];
+ 
+ 
+      for(var i=0; i<arr.length;i++) {
+        while(x<arr.length-1) {
+          var diff = arr[x+1]-arr[x];
+          allObjects.push(diff);
+          x++;
+        }
+ 
+       while(s<allObjects.length) {
+        
+          if(allObjects[s]===allObjects[s+1]) {
+            console.log("entered arit");
+            result = "Arithmetic";
+            
+ 
+          }
+          else if(allObjects[s+1]%allObjects[s]===0) {
+           result = "Geometric";
+           
+          }
+          else {
+            result = '-1';
+            
+          }
+         
+         return result;
+         }
+ 
+ 
+ 
+      }
+ 
+    }
+ 
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+ArithGeo(readline());  
